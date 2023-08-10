af685fd37c28afa80e0fc4e817e5dbdf0c70ab66
yechanlee <firecare@naver.com>
2023-08-10 22:26:46 +0900

    test 2개짜리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Test1123" />
+    <HelloWorld msg="Test1121233" />
   </div>
 </template>
 
